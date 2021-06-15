cc340e35aa74b9bf55762ff88c25e09013886b64
TanmoyPaul1 <pault@bxscience.edu>
2021-06-15 01:56:21 -0400

    Added letter array and starting popup message

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import PySimpleGUI as sg
 
 # sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+sg.popup('Welcome to Typer!', 'Click OK to start playing!!', font=("Helvetica", 25))
 
-layout = [
+letters = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+layout = [ 
     # [sg.Titlebar("Title")],
-    # [sg.Sizer(400, 250)],
-    [sg.Text("Click OK to continue")], 
+    [sg.Sizer(200, 150)],
+    [sg.Text("Click OK to continue", size=(30, 1), font=("Helvetica", 55), text_color="black", background_color = 'SkyBlue1')],
     [sg.Button("OK")]
 ]
 
 # Create the window
-window = sg.Window("Test", layout, margins=(200, 250))
+window = sg.Window("Test", layout, margins=(200, 250), background_color='SkyBlue1')
 
 # Create an event loop
 while True:
