b7dc1a0a2fa1973f7bdf0e6ff2d1f8e0dfd6aad0
TanmoyPaul1 <pault@bxscience.edu>
2021-06-15 02:43:02 -0400

    Working random letter generator

--- main.py
+++ main.py
@@ -1,25 +1,42 @@
 import PySimpleGUI as sg
+import random
 
 # sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
-sg.popup('Welcome to Typer!', 'Click OK to start playing!!', font=("Helvetica", 25))
+# sg.popup('Welcome to Typer!', 'Click OK to start playing!!', font=("Helvetica", 25))
 
 letters = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
+index = random.randrange(25)
+letter = letters[index]
+
 layout = [ 
     # [sg.Titlebar("Title")],
-    [sg.Sizer(200, 150)],
-    [sg.Text("Click OK to continue", size=(30, 1), font=("Helvetica", 55), text_color="black", background_color = 'SkyBlue1')],
-    [sg.Button("OK")]
+    # [sg.Sizer(200, 150)],
+    [sg.Text(letter, font=("Helvetica", 75), text_color="black", background_color='LightSkyBlue1', key= "Letters")],
+    # [sg.Listbox(letters = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"), key="Letters")]
+    [sg.Button("NEXT")],
+    [sg.Button("OK")]   
 ]
 
 # Create the window
-window = sg.Window("Test", layout, margins=(200, 250), background_color='SkyBlue1')
+window = sg.Window("Typer", layout, margins=(200, 170), background_color='LightSkyBlue1')
 
 # Create an event loop
 while True:
     event, values = window.read()
+
+    if event == "NEXT":
+        index = random.randrange(25)
+        letter = letters[index]
+        window.Element("Letters").Update(letter)
+
+    
     # End program if user closes window or
     # presses the OK button
     if event == "OK" or event == sg.WIN_CLOSED:
         break
 
 window.close()
+
+# Progress Bar
+# for i in range(1,10000):
+#     sg.one_line_progress_meter('My Meter', i+1, 10000, 'key','Optional message')
